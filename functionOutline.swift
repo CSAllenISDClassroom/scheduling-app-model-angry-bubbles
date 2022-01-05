9eb569fa96082a474598bcf51f0d472079684d6c
Ricky Das <ankit.das@student.allenisd.org>
2022-01-05 15:29:01 -0600

    Created Initial API Outline

---
+++ functionOutline.swift
@@ -0,0 +1,11 @@
+/*
+API 1: SEARCH QUERY
+Used in response to the user typing in the search bar in page 1 of the scheduler app
+ */
+
+//app.get
+// Used after the user typing a character in the search bar
+// - Parameters:
+// Accepts the input from the user as a string
+// - Returns:
+// Returns an array of CourseLongNameCells to display in the table using an SQL Query
