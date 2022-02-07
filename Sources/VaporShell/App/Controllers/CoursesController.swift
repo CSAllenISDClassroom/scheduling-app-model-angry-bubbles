4c6131484e1e9f5d38ca20abbc5c4790425861f9
Ricky Das <ankit.das@student.allenisd.org>
2022-02-07 15:28:04 -0600

    started my implementation

--- Sources/VaporShell/App/Controllers/CoursesController.swift
+++ Sources/VaporShell/App/Controllers/CoursesController.swift
@@ -16,29 +16,48 @@ public class CoursesController {
     ///   * 404 Not found
     ///
     /// Returns: ``Employee``
-    /// 
+    ///
+    
     public func getCourseByCode(_ app: Application) throws {
-        app.get("courses") { req -> Page<Course> in
+        app.get("courses", "filter", ":filterString") { req -> Page<Course> in
+            
+            var arrayFilters = [String]()
+            
+            guard let filterString = req.parameters.get("filterString", as: String.self) else {
+                //TODO: ADD ERROR MESSAGE SPECIFYING THAT FILTERSTRING IS NOT STRING
+                throw Abort(.badRequest)
+            }
 
-            if let semester = try await Course.query(on: req.db)
-                 .filter(\.$semester == semester)
-                 .all()
-            {
-                return semester
+// SOON TO BE DEPRECATED CODE BECAUSE FUNCTION WILL BE USED INSTEAD
+            for charIndex in filterString {
+                if filterStringCount[charIndex] == "=" {
+                    
+                }
+            }
+
+            //equal sign number is the equal sign you want to find i.e. if you want to find the third equal sign, put 3
+            func findEqualSignIndex(equalSignNumber: Int, filterString: String) -> Int {
+                var equalSignsFound = 0
+                for charIndex in filterString.count {
+                    if filterString[charIndex] == "=" {
+                        if equalSignsFound == equalSignNumber - 1 {
+                            return charIndex
+                        } 
+                    }
+                }
             }
             
-            else {
-                let coursesData = try await CourseData.query(on: req.db)
-                  .paginate(for: req)
-                let courses = try coursesData.map{ try Course(courseData : $0) }
-                return courses
-            }
-
-  
-
-
+            /*
+            let semester = req.parameters.get("semester")
+            let semesterCourses = try await CourseData.query(on: req.db)
+              .filter(\CourseData.semester == semester)
+              .paginate(for: req)
+            let courses = try courseData.map{ try Course(courseData: $0) }
+            
+             */
+            return courses
         }
-        
+
         app.get("courses", ":code") { req -> Course in
 
             guard let code = req.parameters.get("code", as: String.self) else {
